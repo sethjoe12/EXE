dbe2f8dca44dacaf5e10ae9d6554ead197f2abc1
karl joseph <82744732+sethjoe12@users.noreply.github.com>
2023-05-18 12:09:58 +0800

    Add files via upload

---
+++ joe2/controllers/securecontroller_test.go
@@ -0,0 +1,76 @@
+package controllers
+
+import (
+	"testing"
+
+	"github.com/gin-gonic/gin"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func TestAllUser(t *testing.T) {
+	type args struct {
+		context *gin.Context
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			AllUser(tt.args.context)
+		})
+	}
+}
+
+func TestGetUserById(t *testing.T) {
+	type args struct {
+		context *gin.Context
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			GetUserById(tt.args.context)
+		})
+	}
+}
+
+func TestUpdateUserById(t *testing.T) {
+	type args struct {
+		context *gin.Context
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			UpdateUserById(tt.args.context)
+		})
+	}
+}
+
+func TestDeleteUserById(t *testing.T) {
+	type args struct {
+		context *gin.Context
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			DeleteUserById(tt.args.context)
+		})
+	}
+}
